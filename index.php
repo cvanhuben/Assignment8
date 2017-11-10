d7b327f645da05c4be706c8cce0585b53706ea44
tarin <tarinrickett@rochester.edu>
2017-11-09 20:25:16 -0500

    convert to index.php

--- index.php
+++ index.php
@@ -225,8 +225,10 @@
 		</form>
 	</div>
 
-	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<footer>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+	</footer>
 
 </body>
 </html>
