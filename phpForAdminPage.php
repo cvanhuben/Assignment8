bb9a925b3845eeaf980c018d291d70eb5d0e29fe
CJ Van Huben <cvanhube@u.rochester.edu>
2017-11-11 13:53:59 -0500

    Made phpForAdminPage a .php
    added in all the code to be copy and pasted into the admin site to be able to read data from the db

---
+++ phpForAdminPage.php
@@ -0,0 +1,41 @@
+<!-- This is to be used at the top of the file -->
+
+<?php
+	// 1. Create a database connection
+	$dbhost = "66.147.242.186";
+	$dbuser = "urcscon3_queens";
+	$dbpass = "coffee1N";
+	$dbname = "urcscon3_queens";
+
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+
+	// 2. Perform database query
+	$query  = "SELECT * ";
+	$query .= "FROM football ";
+	$query .= "ORDER BY id ASC";
+
+
+	$result = mysqli_query($connection, $query);
+	
+
+?>
+<?
+//  Code to iterate through all the rows 
+
+// 3. Use returned data (if any)
+	while($pages = mysqli_fetch_assoc($result)) {
+// output data from each row
+
+?>
+
+<?
+// This pulls the data and publishes it for each column
+php echo $pages["NAMEOFCOLUMN"]; 
+
+?>
+
+
+
+
+
