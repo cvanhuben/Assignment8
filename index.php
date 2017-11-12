1d2a1bceb9ddfa0fe4670805b045dfb388ca0b15
mdooks <mdookhar@u.rochester.edu>
2017-11-12 03:21:21 -0500

    Uploaded links for the image sources

--- index.php
+++ index.php
@@ -46,13 +46,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col s6">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et sem
-					feugiat, feugiat elit eu, pharetra lectus. Mauris nisl lorem, fermentum
-					et molestie id, posuere id dolor. Pellentesque volutpat nisl et venenatis
-					dapibus. Nullam vitae turpis eu est luctus mattis. Cras eget elementum
-					turpis, quis fermentum lacus. Nullam accumsan nec urna in euismod.
-					Praesent at enim vitae elit viverra fermentum. Sed ullamcorper justo at
-					finibus feugiat. Vestibulum vel purus at nisl consequat maximus.</p>
+				<p>As fruits, we know we’re colorful and juicy—but that’s not why we’re here. 
+				Everyday, we aim to provide society with the most nutrients, vitamins, and 
+				uses to be the leading food in everyone’s household. We want to be near you, 
+				whether that’s a farmers market or the local supermarket. In love
+				with a fruit that is a bit unique? We want to bring more of those to the markets.
+				Through this survey, we hope to improve what we do as fruits, and not just
+				be the juicy morsels on display at the market. </p>
 			</div>
 
 			<div class="col s6">
@@ -64,24 +64,31 @@
 		<div class="row">
 			<div class="col s4">
 				<h1>Frequency</h1>
-				<p>Vestibulum ac lobortis magna. Duis lacinia imperdiet sem vestibulum
-					facilisis. Mauris ultrices ullamcorper libero dapibus auctor. Lorem
-					ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel nisi id
-					metus rhoncus dapibus.</p>
+				<p>Knowing how often you eat and use fruits
+					will help us figure out how to position ourselves
+					to make your experience the best. Go shopping
+					once a week for lots of fruit? First, awesome. 
+					Second, we want to make sure your local areas
+					carry the best fruit possible to make that the best
+					part of your week.</p>
 			</div>
 			<div class="col s4">
 				<h1>Use</h1>
-				<p>Vestibulum ac lobortis magna. Duis lacinia imperdiet sem vestibulum
-					facilisis. Mauris ultrices ullamcorper libero dapibus auctor. Lorem
-					ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel nisi id
-					metus rhoncus dapibus.</p>
+				<p>Do you use fruit just for making face masks? 
+					Do you start every morning with a freshly squeezed 
+					glass of orane juice? Or do you just grab a banana
+					during the day as a light, but satisfactory snack?
+					We want to know which fruits are used for what, and
+					hopefully we can make those more accessible in
+					the future with our partners!</p>
 			</div>
 			<div class="col s4">
 				<h1>Preferences</h1>
-				<p>Vestibulum ac lobortis magna. Duis lacinia imperdiet sem vestibulum
-					facilisis. Mauris ultrices ullamcorper libero dapibus auctor. Lorem
-					ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel nisi id
-					metus rhoncus dapibus.</p>
+				<p>Do you prefer to put fruit in your smoothies? If so,
+					which ones? Which is your favorite fruit to eat? Is that
+					the one you buy every week? If not, why? We want to
+					get to the bottom of it and figure out which fruit
+					our consumers love. </p>
 			</div>
 		</div>
 
@@ -101,7 +108,10 @@
     <div class="footer-copyright">
       <div class="container">
       <a class="grey-text text-lighten-4" href="#!">
-				Survey Source
+				Survey source:  <a href="http://www.marketest.co.uk/market-research-questionnaire/482/fruits"> http://www.marketest.co.uk/market-research-questionnaire/482/fruits </a>
+				<br> Images: <a href="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwizz7DFyrjXAhUERSYKHbEJBLMQjRwIBw&url=%2Furl%3Fsa%3Di%26rct%3Dj%26q%3D%26esrc%3Ds%26source%3Dimages%26cd%3D%26cad%3Drja%26uact%3D8%26ved%3D0ahUKEwizz7DFyrjXAhUERSYKHbEJBLMQjRwIBw%26url%3Dhttps%253A%252F%252Fwww.rheumchoice.com%252F%26psig%3DAOvVaw2iojM0NeJFm52kAI334sZR%26ust%3D1510560503872506&psig=AOvVaw2iojM0NeJFm52kAI334sZR&ust=1510560503872506"> Frequency Icon </a>
+				<br> <a href="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiH6-K7zLjXAhXKSiYKHchlBq0QjRwIBw&url=https%3A%2F%2Ficons8.com%2Ficon%2Ftag%2Forange-juice&psig=AOvVaw2u7CEMOF8EpSi28DdGobSj&ust=1510561023790851"> Orange Juice Icon </a>
+				<br> <a href="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjCr9SnzbjXAhVGySYKHV3TDXYQjRwIBw&url=https%3A%2F%2Fgallery.yopriceville.com%2FFree-Clipart-Pictures%2FFruit-PNG%2FFruit_Basket_PNG_Vector_Clipart_Image&psig=AOvVaw2ZIyAdHVLMicFkZuUu3JHa&ust=1510561247366109">Fruit Basket Icon</a>
 			</a>
       </div>
     </div>
