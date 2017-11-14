2a53ddc3dd69128c5d2dc1b6152b516df9440a67
mdooks <mdookhar@u.rochester.edu>
2017-11-14 14:44:49 -0500

    Colors

--- survey.php
+++ survey.php
@@ -33,7 +33,7 @@
 	</div>
 
 	<div class="container center" id="shaper">
-		<h1>Fruit Research Survey</h1>
+		<h1 id="survey-head">Fruit Research Survey</h1>
 		<p>Please answer this survey as honestly as possible, based on your average
 			eating and buying habits.</p>
 
